e1724ffb4fc1833a768a4eef756ac069b21589ab
CarpeNoctem <aj2600@gmail.com>
2018-02-11 06:41:12 +0000

    Separate verboseness of closed ports and banners
    Add -b flag to enable just open port banners without displaying all closed ports.
    The -v flag now only enables display of closed ports.

--- port_scanner.go
+++ port_scanner.go
@@ -17,6 +17,7 @@ var threads = maxprocs
 var host string
 var low_port, high_port int
 var verbose = false
+var grab_banners = false
 var range_supplied = false
 var top_100 = [100]string{"7", "9", "13", "21", "22", "23", "25", "26", "37", "53", "79", "80", "81", "88", "106", "110", "111", "113", "119", "135", "139", "143", "144", "179", "199", "389", "427", "443", "444", "445", "465", "513", "514", "515", "543", "544", "548", "554", "587", "631", "646", "873", "990", "993", "995", "1025", "1026", "1027", "1028", "1029", "1110", "1433", "1720", "1723", "1755", "1900", "2000", "2001", "2049", "2121", "2717", "3000", "3128", "3306", "3389", "3986", "4899", "5000", "5009", "5051", "5060", "5101", "5190", "5357", "5432", "5631", "5666", "5800", "5900", "6000", "6001", "6646", "7070", "8000", "8008", "8009", "8080", "8081", "8443", "8888", "9100", "9999", "10000", "32768", "49152", "49153", "49154", "49155", "49156", "49157"}
 
@@ -74,7 +75,7 @@ func scanner(ports_ch <-chan string, open_ch, closed_ch chan<- string) {
 		if err != nil {
 			closed_ch <- err.Error()
 		} else {
-			if verbose {
+			if grab_banners {
 				conn.SetReadDeadline(time.Now().Add(timeout))
 				banner, err := bufio.NewReader(conn).ReadString('\n')
 				if err == nil {
@@ -92,10 +93,11 @@ func scanner(ports_ch <-chan string, open_ch, closed_ch chan<- string) {
 func init() {
 	flag.Usage = usage
 
-	flag.IntVar(&threads, "n", threads, "number of goroutines to use")
+	flag.IntVar(&threads, "n", threads, "number of worker threads (goroutines) to use")
 	flag.IntVar(&maxprocs, "c", maxprocs, "maximum number of CPU cores to spread the work across")
 	flag.DurationVar(&timeout, "t", timeout, "connection timeout")
-	flag.BoolVar(&verbose, "v", verbose, "print closed port reasons and open port banners")
+	flag.BoolVar(&verbose, "v", verbose, "display closed ports and reasons")
+	flag.BoolVar(&grab_banners, "b", grab_banners, "grab and display banners from open ports")
 
 	flag.Parse()
 
